4b0ecaead538d6802ed978f3d302b8d50fae2965
Akio Satoh <devtunnel@gmail.com>
2020-01-20 21:48:02 -0800

    Added addon-knobs for storybook

--- src/lib/components/button/v1/ButtonComponentV1.stories.js
+++ src/lib/components/button/v1/ButtonComponentV1.stories.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs'
 import { ButtonComponentV1 } from "../../../../lib";
 
 export default {
   title: 'Button',
   component: ButtonComponentV1,
+  decorators: [withKnobs]
 };
 
 export const Basic = () => (
   <ButtonComponentV1
     variant='contained'
     color='primary'
-    label='Hello Button'
+    label={text('Label', 'Click me')}
     onClick={action('clicked')}
-  >
-    Hello Button
-  </ButtonComponentV1>
+    disabled={boolean('Disabled')}
+  />
 )
